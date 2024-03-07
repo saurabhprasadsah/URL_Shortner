ead1d1eed5ede35a60224c1acfcaff5a8098df7e
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-07 16:32:06 +0530

    jwt

--- service/auth.js
+++ service/auth.js
@@ -1,6 +1,8 @@
 const  jwt = require("jsonwebtoken");
 const secret = "Saurabh$@123";
 
+
+//user jwttoken
 function setUser(user){
     return jwt.sign({
         _id: user._id,
@@ -10,6 +12,8 @@ function setUser(user){
     )
 }
 
+
+//user jwtusertoeken
 function getUser(token){
     if(!token) return null;
     try{
