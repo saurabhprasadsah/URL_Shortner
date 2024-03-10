251a1fdda68fc3c010bbf376e901efb6b9ddb86b
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-10 21:14:31 +0530

    authorization

--- routes/staticRoute.js
+++ routes/staticRoute.js
@@ -3,12 +3,32 @@ const { restrictTo } =require("../middlewares/auth");
 const URL = require("../models/url")
 const router = express.Router();
 
+
+
+router.get("/admin/urls", restrictTo(["Admin"]),   async(req,res) =>{
+
+    const allurls = await URL.find({})
+    return res.render("home", {
+        urls:allurls
+    });
+    
+});
+
+
 router.get("/", restrictTo(['NORMAL']), async (req, res) => {
     const allurls =  await URL.find({createdbBy: req.user._id });
     return res.render("home", {
         urls: allurls,
     });
 });
+
+
+
+
+
+
+
+
 
 
 router.get("/signup",(req, res) =>{
