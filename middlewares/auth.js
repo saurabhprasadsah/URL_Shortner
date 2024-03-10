7a4a2a488349fa6c756a49b01572b6005f1b2bd5
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-10 20:04:21 +0530

    authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,17 @@
 const { getUser } = require('../service/auth');
 
+function checkForAuthentication(req,res,next){
+    const authorizationHeadersValue = req.headers["authorization"]
+    req.user = null;
+    if(
+        !authorizationHeadersValue  ||
+        !authorizationHeadersValue.startWith("Bearer")
+    )
+    return next();
+}
+
+
+
 async function restrictToLoggedinUserOnly(req, res, next) {
     const userUid = req.headers['Authorization'];
 
@@ -7,7 +19,7 @@ async function restrictToLoggedinUserOnly(req, res, next) {
     const token = userUid.split("Bearer ")[1];
 
     const user = getUser(token);
-    
+
     if(!user) return res.redirect("/login");
     req.user = user;
     next();
